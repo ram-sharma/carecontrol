5d41979e73bc8103f9bcf1480d26d532543dc147
Ram Sharma <ram.karmadesignstudio@gmail.com>
2015-03-31 15:24:31 -0400

    init rails 3.2

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Carecontrol::Application.config.secret_token = 'd1f4912cd9559d3c1b21bf064e240356110ab03004d1775ae1070eaeeb6862bee1e58857e7397a92ed3de187919fab0709648af057fd4286cfea05272432f28f'
