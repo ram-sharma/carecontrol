711fe17ac7c1c71358e41a3d143a08900a57210c
Ram Sharma <ram.karmadesignstudio@gmail.com>
2015-04-06 04:29:02 -0400

    moved to document ready - custom.js

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,39 +1,3 @@
-$(function() {
-    $('#side-menu').metisMenu();
-    var d = new Date();
-    $("#quickaddLabel")[0].innerHTML = "Quick Add  - " + d.toUTCString();
-});
-
-//Loads the correct sidebar on window load,
-//collapses the sidebar on window resize.
-// Sets the min-height of #page-wrapper to window size
-$(function() {
-    $(window).bind("load resize", function() {
-        topOffset = 50;
-        width = (this.window.innerWidth > 0) ? this.window.innerWidth : this.screen.width;
-        if (width < 768) {
-            $('div.navbar-collapse').addClass('collapse');
-            topOffset = 100; // 2-row-menu
-        } else {
-            $('div.navbar-collapse').removeClass('collapse');
-        }
-
-        height = ((this.window.innerHeight > 0) ? this.window.innerHeight : this.screen.height) - 1;
-        height = height - topOffset;
-        if (height < 1) height = 1;
-        if (height > topOffset) {
-            $("#page-wrapper").css("min-height", (height) + "px");
-        }
-    });
-
-    var url = window.location;
-    var element = $('ul.nav a').filter(function() {
-        return this.href == url || url.href.indexOf(this.href) == 0;
-    }).addClass('active').parent().parent().addClass('in').parent();
-    if (element.is('li')) {
-        element.addClass('active');
-    }
-});
 $(document).ready(function() {
     if($('#dataTables-patients').length != 0) {
       $('#dataTables-patients').DataTable({
@@ -48,4 +12,42 @@ $(document).ready(function() {
               ]
       });
     }
+    $(function() {
+        $('#side-menu').metisMenu();
+        var d = new Date();
+        $("#quickaddLabel")[0].innerHTML = "Quick Add  - " + d.toUTCString();
+    });
+    $(function() {
+        console.log('test');
+    });
+    //Loads the correct sidebar on window load,
+    //collapses the sidebar on window resize.
+    // Sets the min-height of #page-wrapper to window size
+    $(function() {
+        $(window).bind("load resize", function() {
+            topOffset = 50;
+            width = (this.window.innerWidth > 0) ? this.window.innerWidth : this.screen.width;
+            if (width < 768) {
+                $('div.navbar-collapse').addClass('collapse');
+                topOffset = 100; // 2-row-menu
+            } else {
+                $('div.navbar-collapse').removeClass('collapse');
+            }
+
+            height = ((this.window.innerHeight > 0) ? this.window.innerHeight : this.screen.height) - 1;
+            height = height - topOffset;
+            if (height < 1) height = 1;
+            if (height > topOffset) {
+                $("#page-wrapper").css("min-height", (height) + "px");
+            }
+        });
+
+        var url = window.location;
+        var element = $('ul.nav a').filter(function() {
+            return this.href == url || url.href.indexOf(this.href) == 0;
+        }).addClass('active').parent().parent().addClass('in').parent();
+        if (element.is('li')) {
+            element.addClass('active');
+        }
+    });
 });
